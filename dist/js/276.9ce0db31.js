52ef412393ec2605060eeede6ae298369b916584
jgonzaloDev <quinonesramirezjuniorgonzalo@gmail.com>
2025-05-18 21:36:58 -0500

    dist

---
+++ dist/js/276.9ce0db31.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_3_boots_5_socialnet"]=self["webpackChunkvue_3_boots_5_socialnet"]||[]).push([[276],{1276:function(e,n,u){u.r(n),u.d(n,{default:function(){return c}});var r=u(641);function t(e,n,u,t,o,a){return(0,r.uX)(),(0,r.CE)("h2",null,"Profile page")}var o={name:"ProfileView"},a=u(6262);const s=(0,a.A)(o,[["render",t]]);var c=s}}]);
+//# sourceMappingURL=276.9ce0db31.js.map
